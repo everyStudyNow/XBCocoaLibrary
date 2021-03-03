452b854e913d6f3c0ff37a8ca212ed0362348201
xiaobing.jin <xiaobing.jin@geely.com>
2021-03-03 15:10:14 +0800

    更新

---
+++ XBCustomNav.h
@@ -0,0 +1,16 @@
+//
+//  XBCustomNav.h
+//  XBCocoaLibrary
+//
+//  Created by xiaobing on 2021/3/3.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XBCustomNav : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XBCustomNav.m
@@ -0,0 +1,13 @@
+//
+//  XBCustomNav.m
+//  XBCocoaLibrary
+//
+//  Created by xiaobing on 2021/3/3.
+//
+
+#import "XBCustomNav.h"
+
+@implementation XBCustomNav
+
+
+@end
